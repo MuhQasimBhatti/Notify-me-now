7c623b053bdddaaa2b6302028d8dad1b6f634ab0
MuhQasimBhatti <muhammadqasimbhatti4@gmail.com>
2024-01-03 13:23:29 +0500

    :fire:Project Initialized!

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div style={{backgroundColor:'#009AED', color:'white', fontSize:'60px', textAlign:'center', alignContent:'center', justifyItems:'center', height:'100vh', padding:'10%'} }>
+      Notify Me Now
+    </div>
+  )
+}
+
+export default App
