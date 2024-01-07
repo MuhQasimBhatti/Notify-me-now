446f86c49da86ae624dd75550a142dd11d4e3ec1
MuhQasimBhatti <muhammadqasimbhatti4@gmail.com>
2024-01-07 21:19:38 +0500

    Structure Designed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
       <Home/>
-      <Footer/>
     </div>
   )
 }
